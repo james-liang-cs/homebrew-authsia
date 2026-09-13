36fbe6cdcc3aad263529749ff8dab18e043a24a2
James Liang <66460690+jamesliang-cs@users.noreply.github.com>
2026-09-13 12:26:47 +0800

    authsia 1.7.16

--- Casks/authsia.rb
+++ Casks/authsia.rb
@@ -1,8 +1,8 @@
 cask "authsia" do
-  version "1.7.15"
-  sha256 "63174118e655b49b65c3c612fc5c72a3c2ee0155da486bb3a72ddbb91dee70ff"
+  version "1.7.16"
+  sha256 "8b8e61b67750a5f2388a80e568d5b364629c978290b1a625d67ee7bcde9fe703"
 
-  url "https://authsia.clarionstack.com/Authsia/Authsia-#{version}.dmg?v=20260909164115&source=homebrew"
+  url "https://authsia.clarionstack.com/Authsia/Authsia-#{version}.dmg?source=homebrew"
   name "Authsia"
   desc "Local-first secret manager with a CLI, agent JIT approvals, and 2FA"
   homepage "https://authsia.clarionstack.com/"
